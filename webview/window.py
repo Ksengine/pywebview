9f7fa13eb94bde6ac55ec1c7ccec78f2d98b09c4
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-03-31 22:29:58 +0530

    Update window.py

--- webview/window.py
+++ webview/window.py
@@ -1,327 +1,188 @@
-import inspect
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+pywebview is a lightweight cross-platform wrapper around a webview component that allows to display HTML content in its
+own dedicated window. Works on Windows, OS X and Linux and compatible with Python 2 and 3.
+
+(C) 2014-2019 Roman Sirokov and contributors
+Licensed under BSD license
+
+http://github.com/r0x0r/pywebview/
+"""
+
+
 import logging
+import sys
 import os
-from functools import wraps
+import re
+import threading
+from uuid import uuid4
+from proxy_tools import module_property
 
 from webview.event import Event
-from webview.serving import resolve_url
-from webview.util import base_uri, parse_file_type, escape_string, make_unicode, WebViewException
-from .js import css
+from webview.guilib import initialize
+from webview.util import _token, base_uri, parse_file_type, escape_string, make_unicode, escape_line_breaks, WebViewException
+from webview.window import Window
+from .localization import localization as original_localization
+from .wsgi import Routing, StaticFiles, StaticResources
 
+
+__all__ = (
+    # Stuff that's here
+    'start', 'create_window', 'token',
+    # From wsgi
+    'Routing', 'StaticFiles', 'StaticResources',
+    # From event
+    'Event',
+    # from util
+    '_token', 'base_uri', 'parse_file_type', 'escape_string', 'make_unicode',
+    'escape_line_breaks', 'WebViewException',
+    # from window
+    'Window',
+)
 
 logger = logging.getLogger('pywebview')
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[pywebview] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
+log_level = logging.DEBUG if os.environ.get('PYWEBVIEW_LOG') == 'debug' else logging.INFO
+logger.setLevel(log_level)
 
-def _api_call(function, event_type):
+OPEN_DIALOG = 10
+FOLDER_DIALOG = 20
+SAVE_DIALOG = 30
+
+DRAG_REGION_SELECTOR = '.pywebview-drag-region'
+
+guilib = None
+_debug = False
+_user_agent = None
+_multiprocessing = False
+_http_server = False
+
+token = _token
+windows = []
+
+def start(func=None, args=None, localization={}, gui=None, debug=False, http_server=False, user_agent=None):
     """
-    Decorator to call a pywebview API, checking for _webview_ready and raisings
-    appropriate Exceptions on failure.
+    Start a GUI loop and display previously created windows. This function must
+    be called from a main thread.
+
+    :param func: Function to invoke upon starting the GUI loop.
+    :param args: Function arguments. Can be either a single value or a tuple of
+        values.
+    :param localization: A dictionary with localized strings. Default strings
+        and their keys are defined in localization.py.
+    :param gui: Force a specific GUI. Allowed values are ``cef``, ``qt``, or
+        ``gtk`` depending on a platform.
+    :param debug: Enable debug mode. Default is False.
+    :param http_server: Enable built-in HTTP server. If enabled, local files
+        will be served using a local HTTP server on a random port. For each
+        window, a separate HTTP server is spawned. This option is ignored for
+        non-local URLs.
+    :param user_agent: Change user agent string. Not supported in EdgeHTML.
     """
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        event = args[0].loaded if event_type == 'loaded' else args[0].shown
+    global guilib, _debug, _multiprocessing, _http_server, _user_agent
 
-        try:
-            if not event.wait(15):
-                raise WebViewException('Main window failed to start')
+    def _create_children(other_windows):
+        if not windows[0].shown.wait(10):
+            raise WebViewException('Main window failed to load')
 
-            if args[0].gui is None:
-                raise WebViewException('GUI is not initialized')
+        for window in other_windows:
+            guilib.create_window(window)
 
-            return function(*args, **kwargs)
-        except NameError as e:
-            raise WebViewException('Create a web view window first, before invoking this function')
+    _debug = debug
+    _user_agent = user_agent
+    #_multiprocessing = multiprocessing
+    multiprocessing = False # TODO
+    _http_server = http_server
 
-    return wrapper
+    if multiprocessing:
+        from multiprocessing import Process as Thread
+    else:
+        from threading import Thread
 
+    original_localization.update(localization)
 
-def _shown_call(function):
-    return _api_call(function, 'shown')
+    if threading.current_thread().name != 'MainThread':
+        raise WebViewException('This function must be run from a main thread.')
 
+    if len(windows) == 0:
+        raise WebViewException('You must create a window first before calling this function.')
 
-def _loaded_call(function):
-    return _api_call(function, 'loaded')
+    guilib = initialize(gui)
 
+    for window in windows:
+        window._initialize(guilib, multiprocessing, http_server)
 
-class Window:
-    def __init__(self, uid, title, url, html, width, height, x, y, resizable, fullscreen,
-                 min_size, hidden, frameless, easy_drag, minimized, on_top, confirm_close,
-                 background_color, js_api, text_select, transparent):
-        self.uid = uid
-        self.title = make_unicode(title)
-        self.original_url = None if html else url  # original URL provided by user
-        self.real_url = None  # transformed URL for internal HTTP server
-        self.html = html
-        self.initial_width = width
-        self.initial_height = height
-        self.initial_x = x
-        self.initial_y = y
-        self.resizable = resizable
-        self.fullscreen = fullscreen
-        self.min_size = min_size
-        self.confirm_close = confirm_close
-        self.background_color = background_color
-        self.text_select = text_select
-        self.frameless = frameless
-        self.easy_drag = easy_drag
-        self.hidden = hidden
-        self.on_top = on_top
-        self.minimized = minimized
-        self.transparent = transparent
+    if len(windows) > 1:
+        t = Thread(target=_create_children, args=(windows[1:],))
+        t.start()
 
-        self._js_api = js_api
-        self._functions = {}
+    if func:
+        if args is not None:
+            if not hasattr(args, '__iter__'):
+                args = (args,)
+            t = Thread(target=func, args=args)
+        else:
+            t = Thread(target=func)
+        t.start()
 
-        self.closed = Event()
-        self.closing = Event(True)
-        self.loaded = Event()
-        self.shown = Event()
-
-        self.gui = None
-        self._is_http_server = False
-
-    def _initialize(self, gui, http_server):
-        self.gui = gui
-        self.loaded._initialize(False)
-        self.shown._initialize(False)
-        self._is_http_server = http_server
-
-        # WebViewControl as of 5.1.1 crashes on file:// urls. Stupid workaround to make it work
-        if (
-            gui.renderer == "edgehtml" and
-            self.original_url and
-            isinstance(self.original_url, str) and
-            (self.original_url.startswith('file://') or '://' not in self.original_url)
-        ):
-            self._is_http_server = True
-
-        self.real_url = resolve_url(self.original_url, self._is_http_server)
-
-    @property
-    def width(self):
-        self.shown.wait(15)
-        width, _ = self.gui.get_size(self.uid)
-        return width
-
-    @property
-    def height(self):
-        self.shown.wait(15)
-        _, height = self.gui.get_size(self.uid)
-        return height
-
-    @property
-    def x(self):
-        self.shown.wait(15)
-        x, _ = self.gui.get_position(self.uid)
-        return x
-
-    @property
-    def y(self):
-        self.shown.wait(15)
-        _, y = self.gui.get_position(self.uid)
-        return y
-
-    @property
-    def on_top(self):
-        return self.__on_top
-
-    @on_top.setter
-    def on_top(self, on_top):
-        self.__on_top = on_top
-        if hasattr(self, 'gui') and self.gui != None:
-            self.gui.set_on_top(self.uid, on_top)
-
-    @_loaded_call
-    def get_elements(self, selector):
-        # check for GTK's WebKit2 version
-        if hasattr(self.gui, 'old_webkit') and self.gui.old_webkit:
-            raise NotImplementedError('get_elements requires WebKit2 2.2 or greater')
-
-        code = """
-            var elements = document.querySelectorAll('%s');
-            var serializedElements = [];
-
-            for (var i = 0; i < elements.length; i++) {
-                var node = pywebview.domJSON.toJSON(elements[i], {
-                    metadata: false,
-                    serialProperties: true
-                });
-                serializedElements.push(node);
-            }
-
-            serializedElements;
-        """ % selector
-
-        return self.evaluate_js(code)
-
-    @_shown_call
-    def load_url(self, url):
-        """
-        Load a new URL into a previously created WebView window. This function must be invoked after WebView windows is
-        created with create_window(). Otherwise an exception is thrown.
-        :param url: url to load
-        :param uid: uid of the target instance
-        """
-        self.url = url
-        self.real_url = resolve_url(url, self._is_http_server or self.gui.renderer == 'edgehtml')
-
-        self.gui.load_url(self.real_url, self.uid)
-
-    @_shown_call
-    def load_html(self, content, base_uri=base_uri()):
-        """
-        Load a new content into a previously created WebView window. This function must be invoked after WebView windows is
-        created with create_window(). Otherwise an exception is thrown.
-        :param content: Content to load.
-        :param base_uri: Base URI for resolving links. Default is the directory of the application entry point.
-        :param uid: uid of the target instance
-        """
-
-        content = make_unicode(content)
-        self.gui.load_html(content, base_uri, self.uid)
-
-    @_loaded_call
-    def load_css(self, stylesheet):
-        code = css.src % stylesheet.replace('\n', '').replace('\r', '').replace('"', "'")
-        self.gui.evaluate_js(code, self.uid)
-
-    @_shown_call
-    def set_title(self, title):
-        """
-        Set a new title of the window
-        """
-        self.gui.set_title(title, self.uid)
-
-    @_loaded_call
-    def get_current_url(self):
-        """
-        Get the URL currently loaded in the target webview
-        """
-        return self.gui.get_current_url(self.uid)
-
-    @_shown_call
-    def destroy(self):
-        """
-        Destroy a web view window
-        """
-        self.gui.destroy_window(self.uid)
-
-    @_shown_call
-    def show(self):
-        """
-        Show a web view window.
-        """
-        self.gui.show(self.uid)
-
-    @_shown_call
-    def hide(self):
-        """
-        Hide a web view window.
-        """
-        self.gui.hide(self.uid)
-
-    @_shown_call
-    def set_window_size(self, width, height):
-        """
-        Resize window
-        :param width: desired width of target window
-        :param height: desired height of target window
-        """
-        logger.warning('This function is deprecated and will be removed in future releases. Use resize() instead')
-        self.gui.resize(width, height, self.uid)
-
-    @_shown_call
-    def resize(self, width, height):
-        """
-        Resize window
-        :param width: desired width of target window
-        :param height: desired height of target window
-        """
-        self.gui.resize(width, height, self.uid)
-
-    @_shown_call
-    def minimize(self):
-        """
-        Minimize window.
-        """
-        self.gui.minimize(self.uid)
-
-    @_shown_call
-    def restore(self):
-        """
-        Restore minimized window.
-        """
-        self.gui.restore(self.uid)
-
-    @_shown_call
-    def toggle_fullscreen(self):
-        """
-        Toggle fullscreen mode
-        """
-        self.gui.toggle_fullscreen(self.uid)
-
-    @_shown_call
-    def move(self, x, y):
-        """
-        Move Window
-        :param x: desired x coordinate of target window
-        :param y: desired y coordinate of target window
-        """
-        self.gui.move(x, y, self.uid)
-
-    @_loaded_call
-    def evaluate_js(self, script):
-        """
-        Evaluate given JavaScript code and return the result
-        :param script: The JavaScript code to be evaluated
-        :return: Return value of the evaluated code
-        """
-        escaped_script = 'JSON.stringify(eval("{0}"))'.format(escape_string(script))
-        return self.gui.evaluate_js(escaped_script, self.uid)
-
-    @_shown_call
-    def create_file_dialog(self, dialog_type=10, directory='', allow_multiple=False, save_filename='', file_types=()):
-        """
-        Create a file dialog
-        :param dialog_type: Dialog type: open file (OPEN_DIALOG), save file (SAVE_DIALOG), open folder (OPEN_FOLDER). Default
-                            is open file.
-        :param directory: Initial directory
-        :param allow_multiple: Allow multiple selection. Default is false.
-        :param save_filename: Default filename for save file dialog.
-        :param file_types: Allowed file types in open file dialog. Should be a tuple of strings in the format:
-            filetypes = ('Description (*.extension[;*.extension[;...]])', ...)
-        :return: A tuple of selected files, None if cancelled.
-        """
-        if type(file_types) != tuple and type(file_types) != list:
-            raise TypeError('file_types must be a tuple of strings')
-        for f in file_types:
-            parse_file_type(f)
-
-        if not os.path.exists(directory):
-            directory = ''
-
-        return self.gui.create_file_dialog(dialog_type, directory, allow_multiple, save_filename, file_types, self.uid)
-
-    def expose(self, *functions):
-        if not all(map(callable, functions)):
-            raise TypeError('Parameter must be a function')
-
-        func_list = []
-
-        for func in functions:
-            name = func.__name__
-            self._functions[name] = func
-
-            try:
-                params = list(inspect.getfullargspec(func).args) # Python 3
-            except AttributeError:
-                params = list(inspect.getargspec(func).args)  # Python 2
+    guilib.create_window(windows[0])
 
 
-            func_list.append({
-                'func': name,
-                'params': params
-            })
+def create_window(title, url=None, html=None, js_api=None, width=800, height=600, x=None, y=None,
+                  resizable=True, fullscreen=False, min_size=(200, 100), hidden=False,
+                  frameless=False, easy_drag=True,
+                  minimized=False, on_top=False, confirm_close=False, background_color='#FFFFFF',
+                  transparent=False, text_select=False):
+    """
+    Create a web view window using a native GUI. The execution blocks after this function is invoked, so other
+    program logic must be executed in a separate thread.
+    :param title: Window title
+    :param url: URL to load
+    :param width: window width. Default is 800px
+    :param height:window height. Default is 600px
+    :param resizable True if window can be resized, False otherwise. Default is True
+    :param fullscreen: True if start in fullscreen mode. Default is False
+    :param min_size: a (width, height) tuple that specifies a minimum window size. Default is 200x100
+    :param hidden: Whether the window should be hidden.
+    :param frameless: Whether the window should have a frame.
+    :param easy_drag: Easy window drag mode when window is frameless.
+    :param minimized: Display window minimized
+    :param on_top: Keep window above other windows (required OS: Windows)
+    :param confirm_close: Display a window close confirmation dialog. Default is False
+    :param background_color: Background color as a hex string that is displayed before the content of webview is loaded. Default is white.
+    :param text_select: Allow text selection on page. Default is False.
+    :param transparent: Don't draw window background.
+    :return: window object.
+    """
 
-        if self.loaded.is_set():
-            self.evaluate_js('window.pywebview._createApi(%s)' % func_list)
+    valid_color = r'^#(?:[0-9a-fA-F]{3}){1,2}$'
+    if not re.match(valid_color, background_color):
+        raise ValueError('{0} is not a valid hex triplet color'.format(background_color))
+
+    uid = 'master' if len(windows) == 0 else 'child_' + uuid4().hex[:8]
+
+    window = Window(uid, make_unicode(title), url, html,
+                    width, height, x, y, resizable, fullscreen, min_size, hidden,
+                    frameless, easy_drag, minimized, on_top, confirm_close, background_color,
+                    js_api, text_select, transparent)
+
+    windows.append(window)
+
+    if threading.current_thread().name != 'MainThread' and guilib:
+        window._initialize(guilib, _multiprocessing, _http_server)
+        guilib.create_window(window)
+
+    return window
+
+
+@module_property
+def screens():
+    guilib = initialize()
+    screens = guilib.get_screens()
+    return screens
