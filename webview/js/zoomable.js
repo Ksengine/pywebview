99cba697803180a746d12b9b6fad814674a94349
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-04-02 12:50:47 +0530

    Create zoomable.js

---
+++ webview/js/zoomable.js
@@ -0,0 +1,13 @@
+src = """document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) event.preventDefault();
+}, false);
+
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) event.preventDefault();
+  lastTouchEnd = now;
+}, false);
+
+window.addEventListener("wheel", function(e){e.preventDefault();}, {passive: false} );
+"""
